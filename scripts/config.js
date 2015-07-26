9c704a6bda7922fed3ce640e8cedaec00a649a60
Conor Kelly <ckelly@ckellydesign.net>
2015-07-25 18:39:26 -0700

    Including Front End Dependencies

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,22 @@
 require.config({
 	baseUrl: 'init.js',
 	deps: ['require.js'],
-	
+	shim: {
+		'jquery': {
+			exports: '$'
+		},
+		'backbone': {
+			deps: ['underscore', 'jquery'],
+			exports: 'Backbone'
+		},
+		'underscore': {
+			exports: '_'
+		}
+	},
+	paths: {
+		'jquery': '../components/jQuery/jquery-1.11.3.min',
+		'backbone': '../components/backboneJS/backbone-min',
+		'underscore': '../components/underscoreJS/underscore-min'
+	}
+
 });
