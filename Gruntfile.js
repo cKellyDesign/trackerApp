cefbb7822790aad947adf92518596cd7db0ff776
Conor Kelly <ckelly@ckellydesign.net>
2015-07-25 18:16:09 -0700

    Installing Sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,12 +19,38 @@ module.exports = function(grunt) {
 					node_env: 'production'
 				}
 			}
+		},
+
+		// Watch
+		watch: {
+			express: {
+				files: [],
+				tasks: ['express:dev'],
+				options: {
+					spawn: false
+				}
+			}
+		},
+
+		// Sass
+		sass: {
+			dist: {
+				options: {
+					style: 'expanded'
+				},
+				files: {
+					'www/css/main.css': 'styles/main.scss'
+				}
+			}
 		}
+
 	});
-	
+
 	// Load Task Dependencies
 	grunt.loadNpmTasks('grunt-express-sever');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
 
 	// Define Tasks
-	grunt.regesterTastk('default', ['express:dev']);
+	grunt.regesterTastk('default', ['express:dev', 'watch']);
 };
